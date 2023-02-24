c3c7547a990df25bfc36c4069acfdc7dc0ffe1c5
Leon Kuhne <leonkuhne@gmail.com>
2023-02-24 08:45:51 -0500

    testing audio importing

--- src/script.js
+++ src/script.js
@@ -1,9 +1,10 @@
-// config
-let albums = {
-  
+const play = () => {
+  var audio = new Audio('audio/unsorted/tree.mp3');
+  audio.play();
 }
 
 // start
 window.onload = () => {
-  
+  document.getElementById('play')
+    .addEventListener('click', play);
 }
